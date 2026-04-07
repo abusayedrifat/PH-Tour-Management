7bd5310d62cce07fe3d9c37e86c38a288a16ddc7
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2026-04-07 15:55:04 +0600

    catchAsync handler added

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,33 +1,54 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from "express";
 
-import httpStatus from "http-status-codes"
+import httpStatus from "http-status-codes";
 import { UserServices } from "./user.service";
+import catchAsync from "../../utils/catchAsync";
 
 
-const createUser = async (req:Request, res:Response, next: NextFunction) =>{
+// const createUser = async (req: Request, res: Response, next: NextFunction) => {
+//   try {
+//     const user = await UserServices.createUser(req.body);
 
-    try {
-        
-        const user = await UserServices.createUser(req.body)
- 
+//     res.status(httpStatus.CREATED).json({
+//       message: "User created successfully",
+//       user,
+//     });
+//   } catch (err: any) {
 
-        res.status(httpStatus.CREATED).json({
-            message:"User created successfully",
-            user
-        })
-        
-    } 
+//     console.log(err);
+//     next(err);
+//   }
+// };
+
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    catch (err:any) {
-        // eslint-disable-next-line no-console
-        console.log(err);
-        next(err)
-        
-    }
-}
+    const user = await UserServices.createUser(req.body);
 
+    res.status(httpStatus.CREATED).json({
+      message: "User created successfully",
+      user,
+    });
+  },
+);
+
+
+const getALlUsers = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+
+    const user = await UserServices.getAllUsers();
+
+    res.status(httpStatus.ACCEPTED).json({
+      success: true,
+      user,
+    });
+  },
+);
 
 export const UserControllers = {
-    createUser
-} 
+  createUser,
+  getALlUsers,
+};
